f3a74dd359bf03bd7dda44268674b55bae1ba8c3
jSchnitzer1 <khaled.jendi@gmail.com>
2019-08-03 21:56:20 +0200

    added gui + ajax call to AccountBean + loading spinner image

--- src/main/java/com/accounts/web/view/AccountsBean.java
+++ src/main/java/com/accounts/web/view/AccountsBean.java
@@ -1,24 +1,30 @@
 package com.accounts.web.view;
 
-import com.accounts.web.controller.AccountFacade;
+import com.accounts.web.controller.AccountsFacade;
+import org.apache.log4j.PropertyConfigurator;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
 
-@ManagedBean(name = "welcome", eager = true)
-public class AccountsBean {
+@ManagedBean(name = "accountsBean")
+public class AccountsBean implements Serializable {
+    private static final long serialVersionUID = 1L;
     @EJB
-    AccountFacade accountFacade;
+    AccountsFacade accountsFacade;
 
     @PostConstruct
     public void init() {
+        String log4jConfigPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("WEB-INF/log4j.properties");
+        PropertyConfigurator.configure(log4jConfigPath);
     }
 
     public AccountsBean() {
-
     }
-    public String getStatus() {
-        return accountFacade.isAlive() ? "OK 200" : "Server Error";
+
+    public String getCheckAccountFacade(){
+        return accountsFacade.isAlive() ? "OK 200" : "Server Error";
     }
 }
